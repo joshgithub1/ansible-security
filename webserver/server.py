ac7f372ccc45b772a1811563771f53e651fcfe13
jfach <jordan.facibene13@stjohns.edu>
2016-06-24 14:57:47 +0000

    webserver: updated initial server model
    info: based off of expected data from client script

--- webserver/server.py
+++ webserver/server.py
@@ -2,39 +2,57 @@
 
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from optparse import OptionParser
+import json
+import os
+import re
 
 class RequestHandler(BaseHTTPRequestHandler):
         
+    def alphafy(self, a_string):
+	pattern = "[^a-z0-9_]"
+	return re.sub(pattern, "_", a_string, flags=re.IGNORECASE)
+
     def do_POST(self):
         
         request_path = self.path
+	# handle the play endpoint
        	if request_path == '/play':
 	    request_headers = self.headers
             content_length = request_headers.getheaders('content-length')
             length = int(content_length[0]) if content_length else 0
- 	    post_data = self.rfile.read(length)
-	    args_list = post_data.split()
-	    if '/' not in args_list[0]:
-		self.send_response(400)
+ 	    post_data = json.loads(self.rfile.read(length))
+	    print post_data
+	    playbook = post_data['playbook']
+	    if not playbook:
+	    	self.send_response(400, 'playbook not sent')
 		return
-	    branch_and_book = args_list[0].split('/')
-	    branch = branch_and_book[0]
-	    playbook = branch_and_book[1]
-	    if not (playbook.endswith('.yml') or playbook.endswith('.yaml')):
-		self.send_response(400, "bad playbook extension") # playbooks must have one of these exts
+	    if not playbook.endswith('.yml'):
+	    	self.send_response(400, 'bad playbook extension')
 		return
-	    print "Branch Name: " + branch
-            print "Playbook Name: " + playbook
-            flags = []
-            for arg in args_list:
-		if arg[0] == '-':
-		    flags.append(arg)
-	    print args_list
+	    flags = post_data['flags']
 	    print flags
-            self.send_response(200)
-        else:
-	    self.send_response(409)
-        
+	    directory = post_data['git_handle'] + '/' + post_data['branch_name']
+	    safe_dir = self.alphafy(directory)
+	    print safe_dir
+	    print playbook
+	    command = "ansible-playbook"
+	    if flags:
+	    	for flag in flags:
+		    command += " {0}".format(flag['flag'])
+		    if flag['argument']:
+		    	command += " {0}".format(flag['argument'])
+	    base_dir = os.environ['base_dir']
+            command += " {0}/{1}/{2}".format(base_dir, safe_dir, playbook)
+	    print command
+	    os.system(command)
+	    self.send_response(200)
+	    return
+		    
+	elif request_path == '/run':
+	    return # not implemented yet, TODO
+	    
+	    
+
 def main():
     port = 8080
     print('Listening on localhost:%s' % port)
