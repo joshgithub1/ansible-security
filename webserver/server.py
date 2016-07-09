227dc76ad457c0325b859364a9e32bc776236518
Paul Morgan <jumanjiman@gmail.com>
2016-07-09 16:08:35 +0000

    webserver: break up request handler into methods for clarity
    Fix indentation to resolve cleanerbot/ansible-security/issues/98.

--- webserver/server.py
+++ webserver/server.py
@@ -37,19 +37,28 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         # handle the play endpoint
         if request_path == '/play':
-            request_headers = self.headers
-            content_length = request_headers.getheaders('content-length')
-            length = int(content_length[0]) if content_length else 0
-            post_data = json.loads(self.rfile.read(length))
-            # logging
-            print post_data
-            playbook = post_data['playbook']
+            self.play
+        elif request_path == '/run':
+            self.run
+
+    def play(self):
+        request_headers = self.headers
+        content_length = request_headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        post_data = json.loads(self.rfile.read(length))
+        # logging
+        print post_data
+
+        playbook = post_data['playbook']
+
         if not playbook:
             self.send_response(400, 'playbook not sent')
             return
+
         if not playbook.endswith('.yml'):
             self.send_response(400, 'bad playbook extension')
             return
+
         flags = post_data['flags']
 
         # logging
@@ -72,6 +81,7 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         print playbook
         command = "ansible-playbook"
+
         if flags:
             for flag in flags:
                 command += " {0}".format(flag['flag'])
@@ -83,22 +93,27 @@ class RequestHandler(BaseHTTPRequestHandler):
         base_dir = os.environ['base_dir']
         safe_dir = self.alphafy(directory)
         command += " {0}/{1}/{2}".format(base_dir, safe_dir, playbook)
+
         # Use this to set ANSIBLE_HOSTS environment variable as base_dir, safe_dir
         path = os.path.join(base_dir, safe_dir)
         os.chdir(path)
         os.putenv('ANSIBLE_HOSTS', path + '/'"hosts")
+
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
         (out, err) = proc.communicate()
+
         # logging
         # use os.system(command) in favor of subprocess
         print "program output:", out
         print (command)
         print path
+
         # test pass string output from proc
         #
         self.send_response = (200, out)
         return
-    elif request_path == '/run':
+
+    def run(self):
         return  # not implemented yet, TODO
 
 
