608afcb2c82a3cd2be5d1ae684d89d2ddff2afe7
Paul Morgan <jumanjiman@gmail.com>
2016-07-10 17:42:16 +0000

    move pyfiles into unit test fixture
    This makes the test harness DRYer and allows any unit test
    to access the list of python files.

--- test/unit/fixture.py
+++ test/unit/fixture.py
@@ -15,3 +15,12 @@ class CleanerBotTestFixture(unittest.TestCase):
         print
         print("Testing in: %s" % REPO_DIR)
         print
+        self.files = self.find_py_files(REPO_DIR)
+
+    def find_py_files(self, dirname):
+        pyfiles = []
+        for root, dirs, files in os.walk(dirname):
+            for file in files:
+                if file.endswith('.py'):
+                    pyfiles.append(os.path.join(root, file))
+        return pyfiles
--- test/unit/flake8_tests.py
+++ test/unit/flake8_tests.py
@@ -1,14 +1,10 @@
 #!/usr/bin/envpython
 
 import flake8.main
-import os
-from unit.fixture import CleanerBotTestFixture, REPO_DIR
+from unit.fixture import CleanerBotTestFixture
 
 
 class TestFlake8(CleanerBotTestFixture):
-    def setUp(self):
-        self.files = self.find_py_files(REPO_DIR)
-
     def test_flake8(self):
         style_guide = flake8.main.get_style_guide(paths=self.files)
         report = style_guide.check_files()
@@ -16,11 +12,3 @@ class TestFlake8(CleanerBotTestFixture):
         if report.total_errors != 0:
             output = "\n".join(report.get_statistics())
             self.fail(output)
-
-    def find_py_files(self, dirname):
-        pyfiles = []
-        for root, dirs, files in os.walk(dirname):
-            for file in files:
-                if file.endswith('.py'):
-                    pyfiles.append(os.path.join(root, file))
-        return pyfiles
