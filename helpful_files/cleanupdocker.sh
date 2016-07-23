f2d05a030118334bb03f0013f8317bfc8c688cc5
sometheycallme <timkropp77@gmail.com>
2016-07-23 01:51:18 +0000

    client: add cleanup script for client

--- helpful_files/cleanupdocker.sh
+++ helpful_files/cleanupdocker.sh
@@ -11,32 +11,36 @@ echo "autostager and ansible runnnin containers"
 echo "if there are no PIDS - it's cleaned "
 echo "========================================="
 as_containers=($(docker ps -a | grep autostager | awk '{print $1}'))
-ac_containers=($(docker ps -a | grep ansible | awk '{print $1'}))
+ac_containers=($(docker ps -a | grep ansible | awk '{print $1}'))
+cliet_containers=($(docker ps -a | grep client | awk '{print $1}'))
 as_container_pids=" ${as_containers[*]} "
 ac_container_pids=" ${ac_containers[*]} "
-as_data=($(docker ps -a | grep staging-data | awk '{print $1'}))
+client_pids=" ${client_containers[*]} "
+as_data=($(docker ps -a | grep staging-data | awk '{print $1}'))
 
 echo "====================================="
-echo "finding autostager and ansible images"
-echo "if there are no images - it's cleaned "
+echo "finding pipeline  images             "
+echo "if there are no images - it's cleaned"
 echo "====================================="
 as_images=($(docker images | grep autostager | awk '{print $1}'))
 ac_images=($(docker images | grep ansible | awk '{print $1}'))
+client_images=($(docker images | grep client | awk '${print $1}'))
 as_image_ids=" ${as_images[*]} "
 ac_image_ids=" ${ac_images[*]} "
-
+client_image_ids=" ${client_images[*]} "
 echo "=================================="
 echo "containers that will be cleaned up"
 echo "=================================="
 echo $as_container_pids
 echo $ac_container_pids
-
+echo $client_pids
 echo "=============================="
 echo "images that will be cleaned up"
 echo "=============================="
 echo $as_image_ids
 echo $ac_image_ids
 echo $as_data
+echo $client_pids
 
 echo $as_container_pids | xargs -r $rmf
 echo $ac_container_pids | xargs -r $rmf
