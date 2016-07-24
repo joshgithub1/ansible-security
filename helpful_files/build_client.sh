98d62f3bfda4547c23099c3c4c82e8ce4f8f8310
sometheycallme <timkropp77@gmail.com>
2016-07-24 20:30:06 +0000

    mock-client: add build_client script.

--- helpful_files/build_client.sh
+++ helpful_files/build_client.sh
@@ -5,6 +5,4 @@ CLIENT_IMAGE="client"
 echo "Creating Mock Client"
 echo "==========================================================="
 docker build -f dockerfiles/dockerfile-client -t $CLIENT_IMAGE .
-
-echo "Run and enter Client"
-docker run -i -t --entrypoint bash $CLIENT_IMAGE
+# docker run -i -t --entrypoint bash client
