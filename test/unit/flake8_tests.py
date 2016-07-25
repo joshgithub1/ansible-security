8227a301f9ec59f3f322a9db2484f8ca44f1b7a5
sometheycallme <timkropp77@gmail.com>
2016-07-25 23:06:31 +0000

    flake8: version 3.0.1 test #118 use legacy.api flake8

--- test/unit/flake8_tests.py
+++ test/unit/flake8_tests.py
@@ -2,11 +2,11 @@
 
 import flake8.main
 from unit.fixture import CleanerBotTestFixture
-
+from flake8.api import legacy as flake8
 
 class TestFlake8(CleanerBotTestFixture):
     def test_flake8(self):
-        style_guide = flake8.main.get_style_guide(paths=self.files)
+        style_guide = flake8.get_style_guide(paths=self.files)
         report = style_guide.check_files()
 
         if report.total_errors != 0:
